3ecefd3d41e77420a3384b301409eedd9d3ff51e
Laurent Bovet <laurent.bovet@windmaster.ch>
2010-03-24 17:46:48 +0000

    Added accumulator

---
+++ wfrender/datasource/accumulator.py
@@ -0,0 +1,29 @@
+## Copyright 2009 Laurent Bovet <laurent.bovet@windmaster.ch>
+##                Jordi Puigsegur <jordi.puigsegur@gmail.com>
+##
+##  This file is part of wfrog
+##
+##  wfrog is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class AccumulatorDatasource(object):
+    '''
+    Calculates data from a storage in an iterative fashion by traversing
+    only recently added data.
+
+    [ Properties ]
+
+    storage [storage]:
+        The underlying storage to get samples.
+
