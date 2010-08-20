320038600b22338a2eecef915931ef24a61a4ad3
Laurent Bovet <laurent.bovet@windmaster.ch>
2010-08-20 21:40:12 +0000

    Automated packaging tools

---
+++ pkg/make-release.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+
+repo=https://wfrog.googlecode.com/svn/
+trunk=$repo/trunk
+vfile=wfcommon/config.py
+vline='wfrog_version = "\([^"]*\)"'
+app=wfrog
+checkout=$app
+
+echo "Please make sure the version number is correctly set in $app/$vfile"
+echo Press enter to proceed with checkout or Ctrl-C to abort.
+read key
+
+cd build
+
+echo Checking out $repo ...
+[ "$checkout" != "" ] && rm -fr $checkout*
+svn export $trunk $checkout
+
+# Make the debian dir committable
+rm -fr $checkout/debian
+svn co $trunk/debian $checkout/debian
+
+version=$(grep "$vline" $app/$vfile | sed "s/$vline/\\1/")
+
+echo
+echo '>>> Version is' $version
+echo 
+echo Press enter to proceed with package build or Ctrl-C to abort.
+read key
+
+tag=$repo/tags/$app-$version
+archive=$app-$version.tgz
+
+echo Building archive $archive
+mv $checkout $checkout-$version
+checkout=$checkout-$version
+
+echo
+echo "Press enter description line for this release:"
+read summary
+
+tar cf $archive $checkout
+
+echo
+echo "Please enter the build number [1]"
+read build
+
+if [ "$build" = "" ]; then
+    build=1
+fi
+
+echo Creating debian archive
+
+debversion=$version-$build
+( cd $checkout; dch -v $debversion "$summary")
+( cd $checkout; dch -a "See http://code.google.com/p/wfrog/wiki/ReleaseNotes")
+( cd $checkout; dpkg-buildpackage )
+debarchive=${app}_${debversion}_all.deb
+
+echo Creating RPM archive
+
+mkdir rpm
+cp $debarchive rpm
+(cd rpm; alien --to-rpm $debarchive)
+rpmarchive=$(cd rpm; ls *.rpm)
+mv rpm/$rpmarchive .
+rm -fr rpm
+
+echo
+echo 'Version is' $version
+echo 
+echo Press enter to proceed with package subversion tag or Ctrl-C to abort.
+read key
+
+echo svn ci $checkout/debian/changelog -m"[make-release] Release $app $version"
+
+echo svn copy https://wfrog.googlecode.com/svn/trunk https://wfrog.googlecode.com/svn/tags/wfrog-$version -m"[make-release] Release $app $version"
+
+echo
+echo 'Version is' $version
+echo 
+echo Press enter to proceed with package upload on googlecode or Ctrl-C to abort.
+read key
+
+
+echo python googlecode_upload.py -s"$summary" -p $app $archive
+echo python googlecode_upload.py -s"$summary" -p $app $debarchive
+echo python googlecode_upload.py -s"$summary" -p $app $rpmarchive
+
