b36fd91a1bdf69036720912774a09108bf81e380
laurent.bovet <laurent.bovet@95af4796-acfa-11de-8c46-7bd654735883>
2010-09-07 19:57:41 +0000

    Added timestamp for historic records. See Issue 33.

--- trunk/wfdriver/station/base.py
+++ trunk/wfdriver/station/base.py
@@ -22,13 +22,15 @@ class BaseStation(object):
     New station support should work directly with events.
     '''
 
-    def _report_rain(self, total, rate):
+    def _report_rain(self, total, rate, timestamp=None):
         event = self.generate_event('rain')
         event.total = total
         event.rate = rate
+        if timestamp:
+            event.timestamp = timestamp
         self.send_event(event)
 
-    def _report_wind(self, dirDeg, avgSpeed, gustSpeed):
+    def _report_wind(self, dirDeg, avgSpeed, gustSpeed, timestamp=None):
         event = self.generate_event('wind')
         event.create_child('mean')
         event.mean.dir = dirDeg
@@ -36,25 +38,35 @@ class BaseStation(object):
         event.create_child('gust')
         event.gust.dir = dirDeg
         event.gust.speed = gustSpeed
+        if timestamp:
+            event.timestamp = timestamp        
         self.send_event(event)
 
-    def _report_barometer_absolute(self, pressure):
+    def _report_barometer_absolute(self, pressure, timestamp=None):
         event = self.generate_event('press')
         event.value = pressure
+        if timestamp:
+            event.timestamp = timestamp        
         self.send_event(event)
 
-    def _report_temperature(self, temp, humidity, sensor):
+    def _report_temperature(self, temp, humidity, sensor, timestamp=None):
         event = self.generate_event('temp')
         event.sensor = sensor
         event.value = temp
+        if timestamp:
+            event.timestamp = timestamp        
         self.send_event(event)
 
         event = self.generate_event('hum')
         event.sensor = sensor
         event.value = humidity
+        if timestamp:
+            event.timestamp = timestamp        
         self.send_event(event)
 
-    def _report_uv(self, uv_index):
+    def _report_uv(self, uv_index, timestamp=None):
         event = self.generate_event('uv')
         event.value = uv_index
+        if timestamp:
+            event.timestamp = timestamp           
         self.send_event(event)
