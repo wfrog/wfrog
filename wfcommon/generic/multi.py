706716000b0faa34469f9e85d2d0085f1a05df05
Jordi Puigsegur <jordi.puigsegur@gmail.com>
2011-01-20 21:49:02 +0000

    Correct bug that provoked race condition in slow machines (e.g. ALIX 3D2) and mixes threads (when run in parallel mode).

--- wfcommon/generic/multi.py
+++ wfcommon/generic/multi.py
@@ -55,11 +55,9 @@ class MultiElement(wrapper.ElementWrapper):
             self.logger.debug("Calling "+attr+" on child "+name)
 
             if self.parallel:
-                thread = Thread( target=lambda : r.__getattribute__(attr).__call__(*args, **keywords) )
+                thread = Thread( target = r.__getattribute__(attr).__call__(*args, **keywords) )
                 self.threads.append(thread)
                 thread.start()
-                # Necessary in slow machines to prevent a race condition (probably some bug in threading library)
-                time.sleep(2) 
             else:
                 result[name] = r.__getattribute__(attr).__call__(*args, **keywords)
 
