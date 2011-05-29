9ac2a284d0b1ff1c84365f9f485ee50afa51e474
Robin Kluth <commi1993@gmail.com>
2011-05-29 12:20:13 +0000

    * Added station installer * Updated linklist (Zadig) * Added zadig config * Minor bugfixes
    * WMRS200 USB support!!!

--- pkg/windows/links.bat
+++ pkg/windows/links.bat
@@ -1,4 +1,4 @@
-set link_version=2
+set link_version=3
 
 set pkg_python=http://python.org/ftp/python/2.6.6/python-2.6.6.msi
 set pkg_sliksvn=http://www.sliksvn.com/pub/Slik-Subversion-1.6.16-win32.msi
@@ -14,3 +14,5 @@ set pkg_lxml=http://pypi.python.org/packages/2.6/l/lxml/lxml-2.3-py2.6-win32.egg
 set pkg_pyserial=http://sourceforge.net/projects/pyserial/files/pyserial/2.5/pyserial-2.5.win32.exe/download
 set pkg_pyusb="http://sourceforge.net/projects/pyusb/files/PyUSB 0.x/0.4.3/pyusb-0.4.3.win32-py2.6.exe/download"
 set pkg_namemapper=http://feisley.com/python/cheetah/pyd2.2.1/py26/_namemapper.pyd
+
+set tool_zadig=http://sourceforge.net/projects/libwdi/files/zadig/zadig_v1.1.1.137.7z/download
