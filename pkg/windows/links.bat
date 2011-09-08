5f10ebac4d9201933b5e4a899b3face8f7359fda
Robin Kluth <commi1993@gmail.com>
2011-09-08 10:10:59 +0000

    Updated some links to newest version

--- pkg/windows/links.bat
+++ pkg/windows/links.bat
@@ -4,10 +4,10 @@ set pkg_python=http://python.org/ftp/python/2.6.6/python-2.6.6.msi
 set pkg_sliksvn=http://www.sliksvn.com/pub/Slik-Subversion-1.6.16-win32.msi
 set pkg_easyinst=http://peak.telecommunity.com/dist/ez_setup.py
 set pkg_cheetah=http://pypi.python.org/packages/source/C/Cheetah/Cheetah-2.2.2.tar.gz
-set pkg_libusb10=http://libusb-winusb-wip.googlecode.com/files/libusb_2011.05.10.7z
+set pkg_libusb10=http://libusb-winusb-wip.googlecode.com/files/libusb_2011.09.07.7z
 
 set pkg_py2exe=http://sourceforge.net/projects/py2exe/files/py2exe/0.6.9/py2exe-0.6.9.win32-py2.6.exe/download
-set pkg_pyyaml=http://pyyaml.org/download/pyyaml/PyYAML-3.09.win32-py2.6.exe
+set pkg_pyyaml=http://pyyaml.org/download/pyyaml/PyYAML-3.10.win32-py2.6.exe
 set pkg_pywws=http://pywws.googlecode.com/files/pywws-11.05_r380.zip
 set pkg_pygchart=http://pygooglechart.slowchop.com/files/download/pygooglechart-0.2.1.win32.exe
 set pkg_lxml=http://pypi.python.org/packages/2.6/l/lxml/lxml-2.3-py2.6-win32.egg
@@ -17,4 +17,4 @@ set pkg_pyusb="http://sourceforge.net/projects/pyusb/files/PyUSB 0.x/0.4.3/pyusb
 set pkg_namemapper=http://feisley.com/python/cheetah/pyd2.2.1/py26/_namemapper.pyd
 set pkg_pywin32=http://sourceforge.net/projects/pywin32/files/pywin32/Build216/pywin32-216.win32-py2.6.exe/download
 
-set tool_zadig=http://sourceforge.net/projects/libwdi/files/zadig/zadig_v1.1.1.137.7z/download
+set tool_zadig=http://sourceforge.net/projects/libwdi/files/zadig/zadig_v1.1.1.140.7z/download
