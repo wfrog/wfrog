6097072da49b92d761c833d8e4793515ad7b2506
Robin Kluth <commi1993@gmail.com>
2013-02-09 12:19:26 +0000

    * libusb0.dll also need to be placed in Python installation dir... weird.

--- pkg/windows/setup.bat
+++ pkg/windows/setup.bat
@@ -3,8 +3,8 @@
 rem Todo:
 
 
-set version=1.2.6
-set date=22.01.2012
+set version=1.2.7
+set date=09.02.2013
 
 set python="%cd:~0,1%:\Python26\"
 set easyinst=%python%Scripts\easy_install.exe
@@ -316,7 +316,9 @@ cd libusb0
 %sev_za% x -y libusb0.zip
 cd libusb-win32-bin-1.2.6.0\bin\x86
 xcopy /E /Y "%CD%\libusb0_x86.dll" %windir%\system32\libusb0.dll
+xcopy /E /Y "%CD%\libusb0_x86.dll" %python%DLLs\libusb0.dll
 xcopy /E /Y "%CD%\libusb0.sys" %windir%\system32\drivers
+xcopy /E /Y "%CD%\libusb0.sys" %python%DLLs
 cd ..\..\..\..\
 echo done..
 echo.
